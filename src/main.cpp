6ed3e12078a058b617ac764afc062ca50dd98417
Kien <kien-wlj@live.nl>
2020-04-21 16:52:58 +0200

    added examples

--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,59 @@
 
 int main()
 {
-    std::cout << "Started program." << std::endl;
+    std::cout << "Add up rowvectors:" << std::endl;
+
+    RowVector rowVectorA(1, 2, 3);
+    RowVector rowVectorB(1, 1, 1);
+    RowVector rowVectorC;
+
+    rowVectorC = rowVectorA + rowVectorB;
+
+    rowVectorC.println();
+
+    //----------------------------------------------------
+    std::cout << "------------------------------" << std::endl;
+    std::cout << "Substract Matrices" << std::endl;
 
     Matrix matrixA(
-        RowVector(1, 2, 3),
-        RowVector(4, 5, 6),
-        RowVector(7, 8, 9)
-    );
-    matrixA.addRowVector(RowVector(5, 0, 5));
+        RowVector(2, 2, 2),
+        RowVector(2, 2, 2),
+        RowVector(2, 2, 2) );
 
-    matrixA = matrixA.transpose();
-    matrixA.println();
+    Matrix matrixB(
+        RowVector(1, 1, 1),
+        RowVector(1, 1, 1),
+        RowVector(1, 1, 1) );
+
+    Matrix matrixC;
+    matrixC = matrixA - matrixB;
+
+    matrixC.println();
+
+
+    //----------------------------------------------------
+    std::cout << "------------------------------" << std::endl;
+    std::cout << "Add rowvector in a matrix" << std::endl;
+
+    Matrix matrixD(
+        RowVector(2, 2, 2),
+        RowVector(2, 2, 2),
+        RowVector(2, 2, 2));
+
+    matrixD.addRowVector(RowVector(1, 2, 3));
+
+    matrixD.println();
+
+    //----------------------------------------------------
+    std::cout << "------------------------------" << std::endl;
+    std::cout << "Multiply a columnvector by a scalar" << std::endl;
+
+    ColumnVector columnVectorA(2, 2);
+    columnVectorA = columnVectorA * 5;
+    columnVectorA.println();
 
     return 0;
 }
+
+// clear; make && make run
+// clear; make test
